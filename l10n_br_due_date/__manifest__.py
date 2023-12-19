3ab5c3026b58dfd2ff52874322ff2fa2acadc055
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2023-12-19 13:07:13 -0300

    Criação do Modulo

---
+++ l10n_br_due_date/__manifest__.py
@@ -0,0 +1,18 @@
+
+
+{
+    "name": "Invoice date",
+    "summary": "viewing the invoice date in the dd/mm/yyyy pattern",
+    "license": "AGPL-3",
+    "author": "Engenere",
+    "maintainers": ["cristianomafrajunior"],
+    "website": "https://github.com/Engenere/engenere-addons",
+    "version": "14.0.0.0.0",
+    "depends": [
+        "l10n_br_account",
+    ],
+    "data": [
+        "views/due_date_views.xml",
+    ],
+    "installable": True,
+}
